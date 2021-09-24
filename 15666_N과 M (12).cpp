3895939cb646c3ca20ff9f383a4a3b20597c1c60
Woosung Joung <mung3477@naver.com>
2021-09-24 16:32:01 +0900

    Create 15666_N과 M (12).cpp

---
+++ 15666_N과 M (12).cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using std::cin;
+using std::cout;
+using std::vector;
+using std::sort;
+
+int N, M;
+int* permutation;
+vector<int> numbers;
+int left[10001];
+
+void pickOne(int index, int usableLeastIndex) {
+    if (index <= M) {
+        for (int i = usableLeastIndex; i < numbers.size(); i++) {
+            permutation[index] = numbers.at(i);
+            pickOne(index + 1, i);
+        }
+    }
+    else {
+        for (int i = 1; i <= M; i++) {
+            cout << permutation[i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int input;
+    cin >> N >> M;
+    
+    permutation = new int[M + 1]{};
+    numbers.push_back(-1);
+    
+    for (int i = 1; i <= N; i++) {
+        cin >> input;
+        if (!left[input]) numbers.push_back(input);
+        left[input]++;
+    }
+    
+    sort(numbers.begin(), numbers.end());
+    
+    pickOne(1, 1);
+   
+    return 0;
+}
